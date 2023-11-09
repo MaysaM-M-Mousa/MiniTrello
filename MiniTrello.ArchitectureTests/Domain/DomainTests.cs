b6b1d770e1b40dccc27e9fdb4128c5992fe53606
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-10 01:47:14 +0200

    adding architectural test project and some arch tests

---
+++ MiniTrello.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using MiniTrello.Domain.Primitives;
+using MiniTrello.Domain.Ticket;
+using NetArchTest.Rules;
+using System.Reflection;
+
+namespace MiniTrello.ArchitectureTests.Domain;
+
+public class DomainTests
+{
+    private readonly Assembly DomainAssembly = typeof(Ticket).Assembly;
+    
+    [Fact]
+    public void DomainEvents_ShouldBe_Sealed()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That().ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainEvents_Should_HaveDomainEventPostfix()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That().ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNot_HaveAnyDependency()
+    {
+        var dependencies = new string[]{ "MiniTrello.Application", "MiniTrello.Infrastructure", "MiniTrello.Web" };
+
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOnAll(dependencies)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
