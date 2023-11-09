367bf3e5d65d368f770c5a2778fe81812a3d6029
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-09 20:49:21 +0200

    adding unit tests for update story points/priority

---
+++ MiniTrello.UnitTests/Aggregates/TicketTests/Ticket_UpdatePriority.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using MiniTrello.Domain.Ticket;
+using MiniTrello.Domain.Ticket.DomainEvents;
+using MiniTrello.UnitTests.Aggregates.Builders;
+
+namespace MiniTrello.UnitTests.Aggregates.TicketTests;
+
+public class Ticket_UpdatePriority
+{
+    [Fact]
+    public void UpdatePriorityShouldSucceeds()
+    {
+        var ticket = new TicketBuilder().BuildUnassignedTicket();
+
+        ticket.UpdatePriority(Priority.Hotfix);
+
+        ticket.UncommittedEvents.Count.Should().Be(1);
+        ticket.UncommittedEvents.Single().Should().BeOfType(typeof(TicketPriorityUpdatedDomainEvent));
+    } 
+}
---
+++ MiniTrello.UnitTests/Aggregates/TicketTests/Ticket_UpdateStoryPoints.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using MiniTrello.Domain.Ticket.DomainEvents;
+using MiniTrello.UnitTests.Aggregates.Builders;
+
+namespace MiniTrello.UnitTests.Aggregates.TicketTests;
+
+public class Ticket_UpdateStoryPoints
+{
+    [Fact]
+    public void UpdateStoryPointsShouldSucceeds()
+    {
+        var ticket = new TicketBuilder().BuildUnassignedTicket();
+
+        ticket.UpdateStoryPoints(5);
+
+        ticket.UncommittedEvents.Count.Should().Be(1);
+        ticket.UncommittedEvents.Single().Should().BeOfType(typeof(TicketStoryPointsUpdatedDomainEvent));
+    }
+}
