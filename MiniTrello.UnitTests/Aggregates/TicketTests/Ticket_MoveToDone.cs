843d0d49e0bd19ad5a2a13d9d0b3972a6c1dc8c8
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-09 21:32:24 +0200

    adding moveToTest and moveToDone unit tests

---
+++ MiniTrello.UnitTests/Aggregates/TicketTests/Ticket_MoveToDone.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using MiniTrello.Domain.Ticket.DomainEvents;
+using MiniTrello.Domain.Ticket;
+using MiniTrello.UnitTests.Aggregates.Builders;
+using MiniTrello.Domain.Exceptions;
+
+namespace MiniTrello.UnitTests.Aggregates.TicketTests;
+
+public class Ticket_MoveToDone
+{
+    [Fact]
+    public void MoveFromTestToDoneSucceeds()
+    {
+        var ticket = new TicketBuilder().BuildTestStatusTicket();
+
+        ticket.MoveToDone();
+
+        ticket.Status.Should().Be(TicketStatus.Done);
+        ticket.UncommittedEvents.Count.Should().Be(1);
+        ticket.UncommittedEvents.Single().Should().BeOfType(typeof(TicketMovedToDoneDomainEvent));
+    }
+
+    [Fact]
+    public void MovingFromDoneToDoneDoesNothing()
+    {
+        var ticket = new TicketBuilder().BuildDoneStatusTicket();
+
+        ticket.MoveToDone();
+
+        ticket.Status.Should().Be(TicketStatus.Done);
+        ticket.UncommittedEvents.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void MovingFromCodeReviewToDoneDoesNothing()
+    {
+        var ticket = new TicketBuilder().BuildCodeReviewStatusTicket();
+
+        var act = () => ticket.MoveToDone();
+
+        act.Should()
+            .Throw<MiniTrelloValidationException>()
+            .WithMessage("Only Test tickets can be moved to Done!");
+    }
+}
---
+++ MiniTrello.UnitTests/Aggregates/TicketTests/Ticket_MoveToTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using MiniTrello.Domain.Exceptions;
+using MiniTrello.Domain.Ticket;
+using MiniTrello.Domain.Ticket.DomainEvents;
+using MiniTrello.UnitTests.Aggregates.Builders;
+
+namespace MiniTrello.UnitTests.Aggregates.TicketTests;
+
+public class Ticket_MoveToTest
+{
+    [Fact]
+    public void MoveingFromCodeReviewToTestSucceeds()
+    {
+        var ticket = new TicketBuilder().BuildCodeReviewStatusTicket();
+
+        ticket.MoveToTest();
+
+        ticket.Status.Should().Be(TicketStatus.Test);
+        ticket.UncommittedEvents.Count.Should().Be(1);
+        ticket.UncommittedEvents.Single().Should().BeOfType(typeof(TicketMovedToTestDomainEvent));
+    }
+
+    [Fact]
+    public void MovingFromTestToTestDoesNothing()
+    {
+        var ticket = new TicketBuilder().BuildTestStatusTicket();
+
+        ticket.MoveToTest();
+
+        ticket.Status.Should().Be(TicketStatus.Test);
+        ticket.UncommittedEvents.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void MovingFromToDoToTestFails()
+    {
+        var ticket = new TicketBuilder().BuildAssignedTicket();
+
+        var act = () => ticket.MoveToTest();
+
+        act.Should()
+            .Throw<MiniTrelloValidationException>()
+            .WithMessage("Only CodeReview tickets can be moved to Test!");
+    }
+}
