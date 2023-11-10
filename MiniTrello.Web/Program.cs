b1de5da6859211cbf6da9ccde996ef63aa845afc
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-10 16:03:33 +0200

    referencing infra from web and adding dependencies

--- MiniTrello.Web/Program.cs
+++ MiniTrello.Web/Program.cs
@@ -1,3 +1,6 @@
+using MiniTrello.Infrastructure;
+using MiniTrello.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplicationDependencies()
+    .AddInfrastructureDependencies();
 
 var app = builder.Build();
 
