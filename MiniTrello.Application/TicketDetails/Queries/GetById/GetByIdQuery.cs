bb1b3e144b6d37020d5f5ba612f99ccd32c64d62
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-10 19:56:27 +0200

    adding getProjectioById query

---
+++ MiniTrello.Application/TicketDetails/Queries/GetById/GetByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using MiniTrello.Domain.Ticket.Projections.TicketDetails;
+
+namespace MiniTrello.Application.TicketDetails.Queries.GetById;
+
+public sealed record GetByIdQuery(Guid TicketId) : IRequest<TicketDetailsProjection?>;
---
+++ MiniTrello.Application/TicketDetails/Queries/GetById/GetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MiniTrello.Application.Common.Interfaces;
+using MiniTrello.Domain.Ticket.Projections.TicketDetails;
+
+namespace MiniTrello.Application.TicketDetails.Queries.GetById;
+
+internal sealed class GetByIdQueryHandler : IRequestHandler<GetByIdQuery, TicketDetailsProjection?>
+{
+    private readonly ITicketDetailsProjectionRepository _ticketDetailsProjectionRepository;
+
+    public GetByIdQueryHandler(ITicketDetailsProjectionRepository ticketDetailsProjectionRepository)
+    {
+        _ticketDetailsProjectionRepository = ticketDetailsProjectionRepository;
+    }
+
+    public async Task<TicketDetailsProjection?> Handle(GetByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _ticketDetailsProjectionRepository.GetProjectionByTicketId(request.TicketId);
+    }
+}
