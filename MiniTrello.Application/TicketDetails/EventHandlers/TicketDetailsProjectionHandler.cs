61b96520432d0a6fa61a42b6a69a7d5bfa9e0ff6
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-12 03:40:01 +0200

    adding generic event handler for TicketDetailsProjection

---
+++ MiniTrello.Application/TicketDetails/EventHandlers/TicketDetailsProjectionHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MiniTrello.Application.Common.Interfaces;
+using MiniTrello.Domain.Primitives;
+
+namespace MiniTrello.Application.TicketDetails.EventHandlers;
+
+internal sealed class TicketDetailsProjectionHandler<TDomainEvent> : INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+    private readonly ITicketDetailsProjectionRepository _ticketDetailsProjectionRepository;
+
+    public TicketDetailsProjectionHandler(ITicketDetailsProjectionRepository ticketDetailsProjectionRepository)
+    {
+        _ticketDetailsProjectionRepository = ticketDetailsProjectionRepository;
+    }
+
+    public async Task Handle(TDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var projection = await _ticketDetailsProjectionRepository.GetProjectionByTicketId(notification.AggregateId)
+            ?? new();
+
+        projection.When(notification);
+
+        await _ticketDetailsProjectionRepository.SaveProjection(projection);
+    }
+}
