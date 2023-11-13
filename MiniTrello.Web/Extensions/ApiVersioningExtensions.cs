1002d45e3f82cdb40c178248ab4a5dedffa5fa7d
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-14 01:00:00 +0200

    Adding api versioning

---
+++ MiniTrello.Web/Extensions/ApiVersioningExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniTrello.Web.Extensions;
+
+public static class ApiVersioningExtensions
+{
+    public static IServiceCollection AddMiniTrelloApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(opts =>
+        {
+            opts.AssumeDefaultVersionWhenUnspecified = true;
+            opts.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            opts.ReportApiVersions = true;
+        });
+
+        services.AddVersionedApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+}
--- MiniTrello.Web/Program.cs
+++ MiniTrello.Web/Program.cs
@@ -1,5 +1,6 @@
 using MiniTrello.Infrastructure;
 using MiniTrello.Application;
+using MiniTrello.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMiniTrelloApiVersioning();
 
 builder.Services
     .AddApplicationDependencies()
