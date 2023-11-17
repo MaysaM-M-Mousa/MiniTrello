82192bcae7e2af7c3a22bb0e9dc118caffe919a8
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-17 14:33:21 +0200

    adding contract project

---
+++ MiniTrello.Contracts/Ticket/UpdatePriorityRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTrello.Contracts.Ticket;
+
+public class UpdatePriorityRequest
+{
+    public Guid TicketId { get; set; }
+    
+    public string Priority { get; set; }
+}
---
+++ MiniTrello.Contracts/TicketDetailsProjection/TicketDetailsProjectionResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace MiniTrello.Contracts.TicketDetailsProjection;
+
+public class TicketDetailsProjectionResponse
+{
+    public Guid TicketId { get; set; }
+
+    public string Assignee { get; set; }
+
+    public string Priority { get; set; }
+
+    public int StoryPoints { get; set; }
+
+    public string Status { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime? StartedOnUtc { get; set; }
+
+    public DateTime? CompletedOnUtc { get; set; }
+}
