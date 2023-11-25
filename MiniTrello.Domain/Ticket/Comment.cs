8b7585d3512c7b04438e280095a4b4bad4464557
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-11-25 20:27:31 +0200

    Modifying comment entity - adding ModifiedOnUtc field and adding a static method for creation

--- MiniTrello.Domain/Ticket/Comment.cs
+++ MiniTrello.Domain/Ticket/Comment.cs
@@ -2,13 +2,30 @@
 
 public class Comment
 {
-    public int Id { get; private set; }
+    public long Id { get; private set; }
 
     public Guid TicketId { get; private set; }
 
     public string Text { get; private set; }
 
-    public DateTime DateTime { get; private set; } = DateTime.UtcNow;
-
     public string User { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? ModifiedOnUtc { get; private set; }
+
+    private Comment() { }
+
+    private Comment(Guid ticketId, string text, DateTime createdOnUtc, string user)
+    {
+        TicketId = ticketId;
+        Text = text;
+        User = user;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public static Comment CreateComment(Guid ticketId, string text, string user)
+    {
+        return new Comment(ticketId, text, DateTime.UtcNow, user);
+    }
 }
