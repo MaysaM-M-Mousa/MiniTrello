fae19ad6b533add78dca418e5072559f8a5172e2
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-12-22 20:30:45 +0200

    Adding Result, Error classes

---
+++ MiniTrello.Domain/Primitives/Result/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace MiniTrello.Domain.Primitives.Result;
+
+public sealed record Error(string Code, string? Message = null)
+{
+    public static implicit operator Result(Error error) => Result.Failure(error);
+}
---
+++ MiniTrello.Domain/Primitives/Result/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace MiniTrello.Domain.Primitives.Result;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    private Result(bool isSuccess, Error? error = null)
+    {
+        if ((isSuccess && error != null) || (!isSuccess && error == null))
+        {
+            throw new InvalidOperationException("Invalid Result status");
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Success() => new(true);
+
+    public static Result Failure(Error error) => new(false, error);
+}
