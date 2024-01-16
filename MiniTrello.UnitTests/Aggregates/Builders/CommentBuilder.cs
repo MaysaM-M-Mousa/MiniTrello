a706f47299786aae0ebd3baf48c4a64fdafe9575
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2024-01-16 21:52:24 +0200

    Adding CommentBuilder and CommentBuilderExtensions

---
+++ MiniTrello.UnitTests/Aggregates/Builders/CommentBuilder.cs
@@ -0,0 +1,40 @@
+﻿using MiniTrello.Domain.Primitives;
+using MiniTrello.Domain.Ticket.Comment;
+using MiniTrello.Domain.Ticket.Comment.DomainEvents;
+
+namespace MiniTrello.UnitTests.Aggregates.Builders;
+
+public class CommentBuilder
+{
+    private List<IDomainEvent> _events;
+    private Guid _ticketId;
+    private Guid _commentId;
+
+    public CommentBuilder()
+    {
+        _events = new();
+        _ticketId = Guid.NewGuid();
+        _commentId = Guid.NewGuid();
+    }
+
+    public CommentBuilder AddCommentAddedEvent()
+    {
+        _events.Add(new CommentAddedDomainEvent(_commentId, _ticketId, "Maysam", "Some comment content!"));
+        return this;
+    }
+
+    public CommentBuilder AddCommentDeletedEvent()
+    {
+        _events.Add(new CommentDeletedDomainEvent(_commentId));
+        return this;
+    }
+
+    public Comment Build()
+    {
+        var comment = Comment.Load(_commentId, _ticketId, _events);
+
+        _events.Clear();
+
+        return comment;
+    }
+}
---
+++ MiniTrello.UnitTests/Aggregates/Builders/CommentBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MiniTrello.Domain.Ticket.Comment;
+
+namespace MiniTrello.UnitTests.Aggregates.Builders;
+
+public static class CommentBuilderExtensions
+{
+    public static Comment BuildNewlyAddedComment(this CommentBuilder commentBuilder) =>
+        commentBuilder
+        .AddCommentAddedEvent()
+        .Build();
+
+    public static Comment BuildDeletedComment(this CommentBuilder commentBuilder) =>
+        commentBuilder
+        .AddCommentAddedEvent()
+        .AddCommentDeletedEvent()
+        .Build();
+}
