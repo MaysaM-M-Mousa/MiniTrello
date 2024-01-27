1221c83bce5dca6bfd0b89cde8461c80deb7345e
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2024-01-27 21:19:41 +0200

    Adding comment modified domain event

---
+++ MiniTrello.Domain/Ticket/Comment/DomainEvents/CommentContentModifiedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using MiniTrello.Domain.Primitives;
+
+namespace MiniTrello.Domain.Ticket.Comment.DomainEvents;
+
+public sealed record CommentContentModifiedDomainEvent(Guid AggregateId, string Text) : IDomainEvent
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    Guid IDomainEvent.AggregateId => AggregateId;
+
+    public DateTime OccurredAt { get; set; } =  DateTime.UtcNow;
+}
